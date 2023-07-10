e426af11394706ea6879c49984a2eac210e252c6
chicken-tender <ygm815@naver.com>
2023-07-10 23:00:55 +0900

    :wastebasket: 불필요한 로직 삭제

--- src/main/java/com/kh/backend_finalproject/controller/FestivalController.java
+++ src/main/java/com/kh/backend_finalproject/controller/FestivalController.java
@@ -1,10 +1,8 @@
 package com.kh.backend_finalproject.controller;
 
-import com.kh.backend_finalproject.service.FestivalApiService;
 import com.kh.backend_finalproject.service.FestivalService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,13 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FestivalController {
 
     private final FestivalService festivalService;
-    private final FestivalApiService festivalApiService;
-
-    @GetMapping("/")
-    public ResponseEntity<String> searchFestival() {
-        String festivalData = festivalService.searchFestival();
-        return ResponseEntity.ok(festivalData);
-    }
 
     //  이미지 가져오기
     @GetMapping("/images/{contentId}")
